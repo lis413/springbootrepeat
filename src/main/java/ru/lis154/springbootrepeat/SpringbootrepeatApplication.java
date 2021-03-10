26b60888e028b274b5acffbf7b477ce0e067f467
lis154 <lis154@mail.ru>
2021-03-10 15:37:15 +0300

    Initial commit

---
+++ src/main/java/ru/lis154/springbootrepeat/SpringbootrepeatApplication.java
@@ -0,0 +1,13 @@
+package ru.lis154.springbootrepeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootrepeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootrepeatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/lis154/springbootrepeat/SpringbootrepeatApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lis154.springbootrepeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootrepeatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
